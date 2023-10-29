484c567a629534636fec0c559889832fe6817c5f
RodriDS <98542110+rodridavia@users.noreply.github.com>
2023-10-29 13:53:16 +0100

    [CREATE] Files

---
+++ binary_conversor.py
@@ -0,0 +1,116 @@
+"""
+    Conversor de número entre distintos sistemas númericos creado 
+    en Python con meros fines académicos y de aprendizaje, totalmente 
+    creada de cero y todos los cálculos creados a mano.
+
+    @author: rodrids
+"""
+from conversions import *
+import time
+
+print("#########################################")
+print("######   Conversor de Binarios   ######")
+print("#########################################")
+print("")
+
+while True:
+    
+    conversiones = {
+        1 : "De Decimal a Binario",
+        2 : "De Binario a Decimal",
+        3 : "De Decimal a Octal",
+        4 : "De Octal a Decimal",
+        5 : "De Decimal a Hexadecimal",
+        6 : "De Hexadecimal a Decimal",
+        0 : "Salir"
+    }
+
+    print("----------------------------------------")
+    for clave in conversiones:
+        print(f"{clave} - {conversiones[clave]}")
+    
+    print("----------------------------------------")
+    print("")
+    
+    conversión = int(input("Que conversión quieres hacer: "))
+    
+    print("")
+
+    match conversión:
+        case 1 :
+            print("----------------------------------------")
+            num_conversor = int(input("Introduce el número a convertir: "))    
+            print("")
+            print("               Calculando               ")
+            time.sleep(2)
+            print("")
+            print(f"{num_conversor} es {decimalToBinary(num_conversor)} en binario")
+            print("----------------------------------------")
+            print("\n")
+
+        case 2 :
+            print("----------------------------------------")
+            num_conversor = list(input("Introduce el número a convertir: "))
+            num_org = "".join(num_conversor)
+            print("")
+            print("               Calculando               ")
+            time.sleep(2)
+            print("")
+            print(f"El número {num_org} es {binaryToDecimal(num_conversor)} en decimal")
+            print("----------------------------------------")
+            print("\n")
+
+        case 3 :
+            print("----------------------------------------")
+            num_conversor = int(input("Introduce el número a convertir: "))
+            print("")
+            print("               Calculando               ")
+            time.sleep(2)
+            print("")
+            print(f"{num_conversor} es {decimalToOctal(num_conversor)} en octal")
+            print("----------------------------------------")
+            print("\n")
+
+        case 4 :
+            print("----------------------------------------")
+            num_conversor = list(input("Introduce el número a convertir: "))
+            num_org = "".join(num_conversor)
+            print("")
+            print("               Calculando               ")
+            time.sleep(2)
+            print("")
+            print(f"El número {num_org} es {octalToDecimal(num_conversor)} en decimal")
+            print("----------------------------------------")
+            print("\n")
+
+        case 5 :
+            print("----------------------------------------")
+            num_conversor = int(input("Introduce el número a convertir: "))
+            print("")
+            print("               Calculando               ")
+            time.sleep(2)
+            print("")
+            print(f"{num_conversor} es {decimalToHexadecimal(num_conversor)} en hexadecimal")
+            print("----------------------------------------")
+            print("\n")
+
+        case 6 :
+            print("----------------------------------------")
+            num_conversor = list(input("Introduce el número a convertir: "))
+            num_org = "".join(num_conversor)
+            print("")
+            print("               Calculando               ")
+            time.sleep(2)
+            print("")
+            
+            print(f"El número {num_org} es {hexadecimalToDecimal(num_conversor)} en decimal")
+            print("----------------------------------------")
+            print("\n")
+        case 0 :
+            print("----------------------------------------")
+            print("")           
+            print("Espero que te haya servido - Rodrigo Davia")
+            print("")
+            print("----------------------------------------")
+
+            exit()
---
+++ conversions.py
@@ -0,0 +1,151 @@
+""" 
+    File with all the functions that store the 
+    logic and the algorithms for the code.
+
+    @author: rodrids
+
+ """
+
+def decimalToBinary(dec):
+    coeficiente = dec / 2
+    resto = dec % 2
+    num_convertido = []
+    num_convertido.append(resto)
+
+    while coeficiente >= 1 :
+        resto = int(coeficiente) % 2
+        num_convertido.append(resto)
+        
+        coeficiente = int(coeficiente) / 2
+
+    num_convertido.reverse()
+    num_str = [str(x) for x in num_convertido]
+
+    resultado = "".join(num_str)
+
+    return resultado
+
+def binaryToDecimal(bin):
+    num_conver = [int(x) for x in bin]
+    num_conver.reverse()
+    num_lista = []
+
+    for pos, value in enumerate(num_conver):
+        op = value * (pow(2,pos))
+        num_lista.append(op)
+                        
+    def sum_result(myList):
+        result = 0
+        for x in myList:
+            result = result + x
+        return result
+
+    final = sum_result(num_lista)
+    return final
+
+def decimalToOctal(dec):
+    coeficiente = dec / 8
+    resto = dec % 8
+    num_convertido = []
+    num_convertido.append(resto)
+
+    while coeficiente >= 1 :
+        resto = int(coeficiente) % 8
+        num_convertido.append(resto)
+                
+        coeficiente = int(coeficiente) / 8
+
+    num_convertido.reverse()
+    num_str = [str(x) for x in num_convertido]
+
+    resultado = "".join(num_str)
+
+    return resultado
+
+def octalToDecimal(oct):
+    num_conver = [int(x) for x in oct]
+    num_conver.reverse()
+            
+    num_lista = []
+            
+    for pos, value in enumerate(num_conver):
+        op = value * (pow(8,pos))
+        num_lista.append(op)
+                
+    def sum_result(myList):
+        result = 0
+        for x in myList:
+            result = result + x
+        return result
+
+    final = sum_result(num_lista)
+
+    return final
+
+def decimalToHexadecimal(dec):
+    coeficiente = num_conversor / 16
+    resto = num_conversor % 16
+    num_convertido = []
+    num_convertido.append(resto)
+
+    while coeficiente >= 1 :
+        resto = int(coeficiente) % 16
+        num_convertido.append(resto)
+                
+        coeficiente = int(coeficiente) / 16
+            
+    num_convertido.reverse()
+    num_str = [str(x) for x in num_convertido]
+            
+    for index, value in enumerate(num_str):
+        if value == "10":
+            num_str[index] = "A"
+        if value == "11":
+            num_str[index] = "B"
+        if value == "12":
+            num_str[index] = "C"
+        if value == "13":
+            num_str[index] = "D"
+        if value == "14":
+            num_str[index] = "E"
+        if value == "15":
+            num_str[index] = "F"
+        
+    resultado = "".join(num_str)
+    
+    return resultado
+
+def hexadecimalToDecimal(hex):
+    num_str = [str(x) for x in hex]
+    for index, value in enumerate(hex):
+        if value =="A":
+            hex[index] = 10
+        if value =="B":
+            hex[index] = 11
+        if value =="C":
+            hex[index] = 12
+        if value =="D":
+             hex[index] = 13
+        if value =="E":
+            hex[index] = 14
+        if value =="F":
+            hex[index] = 15
+            
+    num_conver = [int(x) for x in hex]  
+    num_conver.reverse()
+            
+    num_lista = []
+            
+    for pos, value in enumerate(num_conver):
+        op = value * (pow(16,pos))
+        num_lista.append(op)
+            
+    def sum_result(myList):
+        result = 0
+        for x in myList:
+            result = result + x
+        return result
+
+    final = sum_result(num_lista)
+    
+    return final
